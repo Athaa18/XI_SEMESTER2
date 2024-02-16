18e84dc0408f52ef214369fc68ecec9923ccf57f
Athaa18 <athallahdf@gmail.com>
2024-02-16 09:29:19 +0700

    Menampilkan Tulisan dan HTML
    #2

---
+++ PHP_TUGAS/phpsmk-YT/lat01.php
@@ -0,0 +1,11 @@
+<?php 
+
+    echo "Belajar PHP";
+    echo "<br>";
+    echo "Saya Siswa SMK";
+    echo '<h1 style="background-color:red">Belajar PHP Itu Mudah</h1>
+        <p style="background-color:blue">Saya suka belajar progam</p>
+    ';
+    echo 'saya belajar'.' PHP'.'<br>';
+    echo 2019 + 2;
+?>

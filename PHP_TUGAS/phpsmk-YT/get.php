8b1e2b7cf5e64381634a35ca4ffe622d47df3e40
Athaa18 <athallahdf@gmail.com>
2024-02-16 21:10:07 +0700

    GET
    #13

---
+++ PHP_TUGAS/phpsmk-YT/get.php
@@ -0,0 +1,17 @@
+<form action="" method="get">
+
+nama:
+<input type="text" name="nama">
+alamat :
+<input type="text" name="alamat">
+<input type="submit" name="kirim" value="simpan">
+
+</form>
+<?php 
+    
+
+    
+
+
+?>
+

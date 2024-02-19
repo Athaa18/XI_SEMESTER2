e10c49e04a438679342cdb036f99ced6acd507c3
Athaa18 <athallahdf@gmail.com>
2024-02-19 23:01:39 +0700

    Menampilkan Data User Aplikasi Restoran Berbasis Web
    #34

--- PHP_TUGAS/phpsmk-YT/restoran/pelanggan/select.php
+++ PHP_TUGAS/phpsmk-YT/restoran/pelanggan/select.php
@@ -20,9 +20,6 @@
 
 ?>
 
-<div class="float-left mr-4">
-    <a class="btn btn-primary" href="?f=pelanggan&m=insert" role="button">TAMBAH DATA</a>
-</div>
 
 <h3>Pelanggan</h3>
 <table class="table table-bordered w-70 mt-4">

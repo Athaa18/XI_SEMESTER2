ed4e56a86d751834ac80054d88bce89d315c8058
Athaa18 <athallahdf@gmail.com>
2024-02-24 22:55:11 +0700

    Menambah Dan Menghapus Isi Cart Aplikasi Restoran Berbasis Web
    #45

--- PHP_TUGAS/phpsmk-YT/restoran/home/beli.php
+++ PHP_TUGAS/phpsmk-YT/restoran/home/beli.php
@@ -7,6 +7,18 @@
     
 }
 
+if (isset($_GET['tambah'])) {
+    $id=$_GET['tambah'];
+    $_SESSION['_'.$id]++;
+    
+}
+if (isset($_GET['kurang'])) {
+    $id=$_GET['kurang'];
+    $_SESSION['_'.$id]--;
+    if ($_SESSION['_'.$id]==0) {
+    unset($_SESSION['_'.$id]);
+    }
+}
     if (!isset($_SESSION['pelanggan'])) {
         header("location:?f=home&m=login");
     }else {
@@ -32,9 +44,10 @@
 
     function keranjang(){
         global $db;
+        $total=0;
         echo '
         
-        <table class="table table-bordered w-50">
+        <table class="table table-bordered w-70">
             
             <tr>
                 <th>Menu</th>
@@ -56,15 +69,20 @@
                     echo '<tr>';
                     echo '<td>'.$r['menu'].'</td>';
                     echo '<td>'.$r['harga'].'</td>';
-                    echo '<td>'.$value.'</td>';
-                    echo '<td>'.$r['harga']*$value.'</td>';
+                    echo '<td><a href="?f=home&m=beli&tambah='.$r['idmenu'].'">[+]</a> &nbsp &nbsp'.$value.'&nbsp &nbsp<a href="?f=home&m=beli&kurang='.$r['idmenu'].'">[-]</a></td>';
+                    echo '<td>'.$r['harga'] * $value.'</td>';
                     echo '<td><a href="?f=home&m=beli&hapus='.$r['idmenu'].'">Hapus</a></td>';
                     echo '</tr>';
+                    $total=$total+($value * $r['harga']);
                 }
                 
             }
             
         }
+        echo '<tr>
+                <td colspan=4><h3>GRAND TOTAL :</h3></td>
+                <td><h3>'.$total.'</h3></td>
+             </tr>';
         echo '</table>';
     }
 ?>

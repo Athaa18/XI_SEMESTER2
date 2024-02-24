3836aee1895a728bb3b397f51f61f24e2ab8f0b5
Athaa18 <athallahdf@gmail.com>
2024-02-24 15:36:28 +0700

    FRONT END - Mengatur Produk Aplikasi Restoran Berbasis Web
    #40

--- PHP_TUGAS/phpsmk-YT/restoran/home/produk.php
+++ PHP_TUGAS/phpsmk-YT/restoran/home/produk.php
@@ -3,14 +3,23 @@
 
 <div class="mt-4 mb-4">
     <?php 
-        $row=$db->getALL("SELECT * FROM tblkategori ORDER BY kategori ASC");
+       
+       if (isset($_GET['id'])) {
+            $id=$_GET['id'];
+            $where="WHERE idkategori=$id";
+            $id="&id=".$id;
+            
+       }else {
+        $where="";
+        $id="";
+       }
         
     ?>
 
 </div>
 
 <?php 
-    $jumlahdata=$db->rowCOUNT("SELECT idmenu FROM tblmenu");
+    $jumlahdata=$db->rowCOUNT("SELECT idmenu FROM tblmenu $where");
 
     $banyak=3;
 
@@ -23,7 +32,7 @@
         $mulai=0;
     }
 
-    $sql="SELECT * FROM tblmenu  ORDER BY menu ASC LIMIT $mulai,$banyak";
+    $sql="SELECT * FROM tblmenu $where ORDER BY menu ASC LIMIT $mulai,$banyak";
     $row=$db->getALL($sql);
     
     $no=1+$mulai;
@@ -54,7 +63,7 @@
 <?php 
 
     for ($i=1; $i <=$halaman ; $i++) { 
-        echo '<a href="?f=home&m=produk&p='.$i.'">'.$i.'</a>';
+        echo '<a href="?f=home&m=produk&p='.$i.$id.'">'.$i.'</a>';
         echo '&nbsp &nbsp &nbsp';
     }
 

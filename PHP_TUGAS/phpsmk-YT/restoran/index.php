ae2de63e30423e8a17219ee41e5c6b06945ff187
Athaa18 <athallahdf@gmail.com>
2024-02-25 14:04:36 +0700

    Histori Pembelian Aplikasi Restoran Berbasis Web
    #50

--- PHP_TUGAS/phpsmk-YT/restoran/index.php
+++ PHP_TUGAS/phpsmk-YT/restoran/index.php
@@ -52,6 +52,7 @@
                          <div class="float-end mt-4"><a href="?log=logout">Logout</a></div>
                          <div class="float-end mt-4 me-4">Pelanggan :'.$_SESSION['pelanggan'].'</div>
                          <div class="float-end mt-4 me-4">Cart : ( <a href="?f=home&m=beli"> '.cart().'</a>)</div>
+                         <div class="float-end mt-4 me-4"><a href="?f=home&m=history"> History </a></div>
                         ';
                     }else {
                         echo '

c3bee93b3fb776c4de24022c99033217174799b5
Athaa18 <athallahdf@gmail.com>
2024-02-25 14:32:36 +0700

    Admin Page Menampilkan Order Pembelian Aplikasi Restoran Berbasis Web
    #51

--- PHP_TUGAS/phpsmk-YT/restoran/home/beli.php
+++ PHP_TUGAS/phpsmk-YT/restoran/home/beli.php
@@ -62,7 +62,7 @@ if (isset($_GET['kurang'])) {
         
         ';
         foreach ($_SESSION as $key => $value) {
-            if ($key<>'pelanggan' && $key<>'idpelanggan') {
+            if ($key<>'pelanggan' && $key<>'idpelanggan' && $key<>'user' && $key<>'level' && $key<>'iduser') {
                 $id=substr($key,1);
                 $sql="SELECT * FROM tblmenu WHERE idmenu=$id";
                 $row=$db->getALL($sql);
